99f5247e73c5be0a28a77bd45c6b4edf79b9c033
karthick <prog@ecomsense.in>
2022-12-08 02:39:31 +0530

    new git repo scr

---
+++ client/git_new.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ -z "$1" ]; then 
+    echo "you need to provide the repo name"
+else 
+    git init
+    git add .
+    git commit -am "first commit"
+    git remove add origin https://github.com/pannet1/$1.git
+    git branch -M main
+    git push -u origin main
+fi
