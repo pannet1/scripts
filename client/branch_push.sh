66d725245758c24c227a77a6b422995dd933b8a1
karthick <prog@ecomsense.in>
2023-06-18 23:59:35 +0530

    fix branch_push

--- client/branch_push.sh
+++ client/branch_push.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-if [ -z "$1" ]; then 
-    git add .
-    if [ -z "$2" ]; then 
-        git commit -am "no comments"
-    else 
-        git commit -am "$2"
-    fi
-    git push origin "$1"
+if [ -z "$1" ]; then
+	echo "branch name is required"
 else
-  echo "branch name is required"
+	git add .
+	if [ -z "$2" ]; then
+		git commit -am "no comments"
+	else
+		git commit -am "$2"
+	fi
+	git push origin "$1"
+fi
