1aa894c654ad45e9580bf215ee51117b8e060f06
pannet1 <prog@ecomsense.in>
2023-10-24 16:33:13 +0530

    add xsh scripts

--- client/1_xontribs.xsh
+++ client/1_xontribs.xsh
@@ -1,16 +1,16 @@
-#!/bin/xonsh
+#!/bin/env xonsh
 
-xontribs =  ['argcomplete', 'abbrevs', 'avox', 'back2dir', 'broot', 'distributed', 'cmd_done', 'gitinfo', 'hist_navigator', 'histcpy', 'kitty', 'mpl', 'onepath', 'output_search', 'powerline3', 'prompt_vi_mode', 'readable-traceback', 'sh', 'ssh_agent', 'up', 'xlsd','vox', 'z']
+xontribs =  ['argcomplete', 'abbrevs', 'back2dir', 'broot', 'clp', 'debug-tools', 'distributed', 'cmd_done', 'gitinfo', 'gruvbox', 'hist_navigator', 'histcpy', 'kitty', 'mpl', 'onepath', 'output_search', 'powerline3', 'prompt_vi_mode', 'readable-traceback', 'sh', 'ssh_agent', 'up', 'xlsd','vox', 'z']
 xonshes = ['autoxsh', 'autovox', 'vox_tabcomplete']
 
-xpip install pyperclip repassh
+pip install onefetch pyperclip repassh --user --break-system-packages
 
 for item in xontribs:
   item = "xontrib-"+item
-  xpip install @(item)
+  pip install @(item) --user --break-system-packages
 
-for xs in new_items:
+for xs in xonshes:
   xs = "xonsh-"+xs
-  pip install @(xs)
+  pip install @(xs)  --user --break-system-packages
 
 
--- client/1strun.sh
+++ client/1strun.sh
@@ -1,57 +1,22 @@
 qtile
-ttf-cascadia-code
-arc-gtk-theme-eos
-xfce4-terminal
-pcmanfm-gtk3
-picom
-alsa-utils
-papirus-icon-theme
-pasystray
-feh
-rofi
-i3lock
+atool xclip highlight
+ranger pcmanfm-gtk3
+picom lightdm lightdm-gtk-greeter lxappearance
+oasystray i3lock
+alsa-utils amixer alsamixer
+rofi nitrogen
+nerd-fonts-jetbrains-mono nerd-fonts-mononoki nerd-fonts-source-code-pro ttf-dejavu ttf-liberation noto-fonts ttf-cascadia-code
+xorg-server-xephyr
+rofi-theme-selector arc-gtk-theme-eos papirus-icon-theme
 neovim
-virtualbox
-yarn 
-npm
-cargo
-LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
-LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
-nerd-fonts-jetbrains-mono
-nerd-fonts-mononoki
-nerd-fonts-source-code-pro
-ln -s ~/.local/bin/lvim lvim
-xclip
-python3 -m pip install --upgrade pip
-cd /tmp
-git clone https://github.com/EndeavourOS-Community-Editions/qtile.git
-cd qtile
-sudo pacman -S --needed - < packages-repository.txt 
-sudo pacman -S picom nitrogen lightdm 
-sudo pacman -S xorg-server-xephyr
-Xephyr -br -ac -noreset -screen 1280x720 :1 & [200~DISPLAY=:1 qtile "/home/pannet1/.config/qtile/config.py" 
-sudo pip install psutil
-amixer
-alsamixer
+libreoffice-still
+virtualbox virtualbox-guest-iso
+feh
+Xephyr -br -ac -noreset -screen 1280x720 :1 &
+[200~DISPLAY=:1 qtile "/home/pannet1/.config/qtile/config.py"
 echo "add ~/.xprofile with setxkbmap, picom and nitrogen"
-lxappearance
-picom 
-lightdm
-lightdm-gtk-greeter
+rofi -dump-config >~/.config/rofi/config.rasi
+blueman
+sudo systemctl enable bluetooth
+sudo pacman -S --needed - <packages-repository.txt
 sudo systemctl start lightdm
-rofi
-rofi -show run
-mkdir -p ~/.config/rofi
-rofi -dump-config > ~/.config/rofi/config.rasi
-rofi-theme-selector 
-pacman -S ranger
-sudo pacman -S ranger
-ranger 
-atool
-highlight
-w3m
-pacman -Rnsdd xfce4 xfce4-goodies
-alacritty
-thunar
-sudo pacman -S ttf-dejavu ttf-liberation noto-fonts
-
